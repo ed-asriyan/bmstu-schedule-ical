ac1c9f25619af46185bd42b3c9e0bf09aa5c4762
Ed Asriyan <ed-asriyan@protonmail.com>
2017-11-04 17:49:01 +0300

    Implement getSemesterTime

--- bmstu-schedule/getSemesterTime.js
+++ bmstu-schedule/getSemesterTime.js
@@ -4,31 +4,46 @@
 
 'use strict';
 
-const getSemesterTime = function () {
+const getFirstWeekDay = function (year, month, weekDay) {
+    const day = new Date(year, month);
+    while (day.getDay() !== weekDay) {
+        day.setDate(day.getDate() + 1);
+    }
+    return day;
+};
+
+const getSemesterTime = function (timePoint = new Date()) {
+    const isSummer = timePoint.getMonth() < 7;
+
+    const beginYear = timePoint.getFullYear();
+    const endYear = beginYear;
+    const beginMonth = isSummer ? 1 : 8;
+    const endMonth = beginMonth + 3;
+
     return {
-        monday: {
-            first: new Date(2017, 8, 4),
-            last: new Date(2017, 11, 25, 59, 59, 59),
+        'monday': {
+            first: getFirstWeekDay(beginYear, beginMonth, 1),
+            last: new Date(endYear, endMonth + 1, -2, 59, 59, 59),
         },
-        tuesday: {
-            first: new Date(2017, 8, 5),
-            last: new Date(2017, 11, 26, 59, 59, 59),
+        'tuesday': {
+            first: getFirstWeekDay(beginYear, beginMonth, 2),
+            last: new Date(endYear, endMonth + 1, -2, 59, 59, 59),
         },
-        wednesday: {
-            first: new Date(2017, 8, 6),
-            last: new Date(2017, 11, 27, 59, 59, 59),
+        'wednesday': {
+            first: getFirstWeekDay(beginYear, beginMonth, 3),
+            last: new Date(endYear, endMonth + 1, -2, 59, 59, 59),
         },
-        thursday: {
-            first: new Date(2017, 8, 7),
-            last: new Date(2017, 11, 28, 59, 59, 59),
+        'thursday': {
+            first: getFirstWeekDay(beginYear, beginMonth, 4),
+            last: new Date(endYear, endMonth + 1, -2, 59, 59, 59),
         },
-        friday: {
-            first: new Date(2017, 8, 1),
-            last: new Date(2017, 11, 29, 59, 59, 59),
+        'friday': {
+            first: getFirstWeekDay(beginYear, beginMonth, 5),
+            last: new Date(endYear, endMonth + 1, -2, 59, 59, 59),
         },
-        saturday: {
-            first: new Date(2017, 8, 2),
-            last: new Date(2017, 11, 30, 59, 59, 59),
+        'saturday': {
+            first: getFirstWeekDay(beginYear, beginMonth, 6),
+            last: new Date(endYear, endMonth + 1, -2, 59, 59, 59),
         },
     };
 };
