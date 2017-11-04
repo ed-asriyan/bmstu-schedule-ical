2348390007e4c14578c6df6b9e87ae9a763bb570
Ed Asriyan <ed-asriyan@protonmail.com>
2017-11-04 20:32:27 +0300

    Create web page

--- app/index.js
+++ app/index.js
@@ -5,10 +5,28 @@
 'use strict';
 
 import './styles.scss';
-
+import {saveAs} from 'file-saver';
 import {generateICal} from './bmstu-schedule';
 
-(async () => {
-    const calendar = await generateICal('ИУ', '7', '53');
-    document.body.innerHTML = `<textarea>${calendar.toString()}</textarea>`;
-})();
+const button = document.getElementById('btn');
+const groupInput = document.getElementById('group_input');
+
+const parseInput = function (input) {
+    const regex = /([а-яА-ЯЁё]+)(\d+)-(\d+)/;
+    const match = regex.exec(input);
+
+    return {
+        faculty: match[1],
+        department: match[2],
+        groupNumber: match[3],
+    }
+};
+
+button.addEventListener('click', async function () {
+    const input = parseInput(groupInput.value);
+
+    const ics = (await generateICal(input.faculty, input.department, input.groupNumber)).toString();
+
+    const blob = new Blob([ics], {type: "text/calendar;charset=utf-8"});
+    saveAs(blob, `BMSTU ${new Date().getFullYear()} ${input.faculty}${input.department}-${input.groupNumber}.ics`);
+});
