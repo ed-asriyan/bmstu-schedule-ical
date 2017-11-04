3ab26ad9a94330e7bfacf3531b76227eba371e62
Ed Asriyan <ed-asriyan@protonmail.com>
2017-11-04 17:04:32 +0300

    Implement generator

--- bmstu-schedule/generator.js
+++ bmstu-schedule/generator.js
@@ -4,8 +4,71 @@
 
 'use strict';
 
+import ical from 'ical-generator';
+import getPeriodTime from "./getPeriodTime";
+import getSemesterTime from "./getSemesterTime";
+
+const createEvent = function (cal, semesterDayTime, periodTime, studyClass) {
+    const isNormal = studyClass['type'] === 'normal';
+    const nominator = studyClass['type'] === 'nominator';
+
+    const start = new Date(
+        semesterDayTime.first.getFullYear(),
+        semesterDayTime.first.getMonth(),
+        semesterDayTime.first.getDate() + (nominator ? 7 : 0),
+        periodTime.begin.getHours(),
+        periodTime.begin.getMinutes(),
+    );
+
+    const end = new Date(
+        semesterDayTime.first.getFullYear(),
+        semesterDayTime.first.getMonth(),
+        semesterDayTime.first.getDate() + (nominator ? 7 : 0),
+        periodTime.end.getHours(),
+        periodTime.end.getMinutes(),
+    );
+
+    return cal.createEvent({
+        start: start,
+        end: end,
+        summary: studyClass['studyClassTitle'],
+        location: studyClass['studyClassRoom'],
+        description: studyClass['studyClassLecturer'],
+        repeating: {
+            freq: 'WEEKLY',
+            interval: isNormal ? 1 : 2,
+            until: semesterDayTime.last,
+        },
+    });
+};
+
+const handlePeriod = function (cal, semesterDayTime, period) {
+    const periodTime = getPeriodTime(period);
+    for (let studyClass of period['studyClasses']) {
+        createEvent(cal, semesterDayTime, periodTime, studyClass);
+    }
+};
+
+const handleDay = function (cal, semesterTime, day) {
+    const dayName = day['title'];
+    const semesterDayTime = semesterTime[dayName];
+    for (let period of day['periods']) {
+        handlePeriod(cal, semesterDayTime, period);
+    }
+};
+
 const createICal = function (scheduleJson) {
-    return JSON.stringify(scheduleJson);
+    const cal = ical({
+        timezone: 'Europe/Moscow'
+    });
+
+    const semesterTime = getSemesterTime();
+
+    for (let day of scheduleJson[0]['studyWeek']) {
+        handleDay(cal, semesterTime, day);
+    }
+
+    return cal;
 };
 
 export {createICal};
