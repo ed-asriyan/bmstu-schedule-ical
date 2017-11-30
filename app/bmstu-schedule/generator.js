94b55f4bb5b585394f1795aeab65149347ff0807
Ed Asriyan <ed-asriyan@protonmail.com>
2017-11-30 20:08:34 +0300

    Remove timezone

--- app/bmstu-schedule/generator.js
+++ app/bmstu-schedule/generator.js
@@ -8,8 +8,6 @@ import ical from 'ical-generator';
 import getPeriodTime from "./getPeriodTime";
 import getSemesterTime from "./getSemesterTime";
 
-const TIMEZONE = 'Europe/Moscow';
-
 const createEvent = function (cal, semesterDayTime, periodTime, studyClass) {
     const isNormal = studyClass['type'] === 'normal';
     const nominator = studyClass['type'] === 'nominator';
@@ -45,7 +43,6 @@ const createEvent = function (cal, semesterDayTime, periodTime, studyClass) {
         summary: title,
         location: studyClass['studyClassRoom'],
         description: description,
-        timezone: TIMEZONE,
         repeating: {
             freq: 'WEEKLY',
             interval: isNormal ? 1 : 2,
@@ -71,9 +68,7 @@ const handleDay = function (cal, semesterTime, day) {
 
 const createICal = function (scheduleJson) {
 
-    const cal = ical({
-        timezone: TIMEZONE,
-    });
+    const cal = ical();
 
     const semesterTime = getSemesterTime();
 
